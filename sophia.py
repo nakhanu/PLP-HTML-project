1eb990abdb32b96bb6416fecc832d056edd41415
nakhanu <nakhanusophia99@gmail.com>
2025-03-12 13:10:15 +0300

    Basic Calculator Program

--- sophia.py
+++ sophia.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+n1 = float (input('Enter first number '))
+operator = input("Enter the operator (+,-,*,/:) ")
+n2 = float (input('Enter second number '))
+if operator == '+':
+    result = n1 + n2
+elif operator == '-':
+    result = n1 - n2
+elif operator == '*':
+    result = n1 * n2
+elif operator == '/':
+    if n2 != 0:
+        result = n1/n2
+    else:
+        result = "Error!"
+else:
+    result = "Invalid operator"
+print (f"{n1} {operator} {n2} = {result}")
+
+
+
